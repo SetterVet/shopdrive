f45c120a548ceef1bd120387b35ece356951103e
SetterVet <kosvit1408@gmail.com>
2018-10-26 19:07:33 +0300

    add room.html and room

--- shopdrive/shop/views.py
+++ shopdrive/shop/views.py
@@ -6,16 +6,20 @@ import json
 
 
 # Create your views here.
+def profile_item(request,userid):
+    current_user=User.objects.get(pk=userid)
+    return render(request, 'shop/profileuser.html',{'user':current_user})
+
 def shophome(request):
     if request.method=='POST':
         postemail=request.POST['mail']
-        user=User.objects.get(email=postemail)
-
-        if user.email==postemail:
+        try:
+            user=User.objects.get(email=postemail)
             print(user)
-            return HttpResponse('<h1>Find User</h1>')
-
-
+            return profile_item(request,user.pk)
+        except User.DoesNotExist:
+            content="This email not in database . Please register"
+            return render(request,'shop/index.html',{'content':content})
 
     return render(request, 'shop/index.html')
 
@@ -24,3 +28,15 @@ def shophome(request):
 def shopgood(request):
     goods=Good.objects.all()
     return render(request, 'shop/goodlist.html', {'goods': goods})
+
+def room(request):
+    if request.method=='POST':
+        postemail=request.POST['mail']
+        try:
+            user=User.objects.get(email=postemail)
+            print(user)
+            return profile_item(request,user.pk)
+        except User.DoesNotExist:
+            content="This email not in database . Please register"
+            return render(request,'shop/room.html',{'content':content})
+    return render(request, 'shop/room.html')
